07f2a1375afa1a1d57cb648fe1a2f93f36e42857
Andrew Fakher <andrew_fakher96@hotmail.com>
2020-03-07 22:24:45 +0200

    second commit

--- Geocoding/ViewController.swift
+++ Geocoding/ViewController.swift
@@ -7,14 +7,114 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
+    var previousLocation: CLLocation?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        checkLocationServices()
     }
-
-
+    
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    func centerViewOnUserLocation(){
+        if let location = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//                        let region = MKCoordinateRegion.init(center: location, span: MKCoordinateSpan(latitudeDelta: regionInMeters, longitudeDelta: regionInMeters))
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        }
+        else {
+            
+        }
+    }
+    
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus(){
+        case .authorizedWhenInUse:
+            startTrackingUserLocation()
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
+    func startTrackingUserLocation(){
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation{
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    @IBAction func pickLocation(_ sender: Any) {
+        print(addressLabel.text ?? "")
+    }
+    
+    @IBAction func getToTheCurrentLocation(_ sender: Any) {
+        checkLocationServices()
+    }
+    
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+    
+}
+
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        addressLabel.text = "Loading..."
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else {return}
+        guard center.distance(from: previousLocation) > 50 else {return}
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks,error) in
+            guard let self = self else {return}
+            
+            if let _ = error {
+                return
+            }
+            guard let placemark = placemarks?.first else {
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            let country = placemark.country ?? ""
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetNumber) \(streetName), \(country)"
+            }
+        }
+    }
+}
